baee80870132a4ca2bfacb47437676c866a8a9a3
Mxintro <986695652@qq.com>
2021-06-14 23:47:53 +0800

    add store

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import { GlobalStyle } from './style';
 import { renderRoutes } from 'react-router-config';
 import routes from './routes/index'
 import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store/index'
 
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      { renderRoutes (routes) }
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        { renderRoutes (routes) }
+      </HashRouter>
+    </Provider>
   );
 }
 
